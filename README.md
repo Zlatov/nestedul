9a5310b85255a014669dd351d6109bdd144b5bef
zlatov <zlatov@ya.ru>
2025-03-22 17:47:41 +0300

    Пересоздаём dist из src, пока без изображений;

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ```sh
 npm init
+
+# Пересоздаём dist из src
+rm -r dist &&\
+mkdir -p dist/js &&\
+uglifyjs src/js/nlist.js --comments '/^!|@(?:license|preserve)/' -b indent_level=2 -o dist/js/nlist.js &&\
+uglifyjs src/js/nlist.js -m --comments '/^!|@(?:license|preserve)/' -o dist/js/nlist.min.js &&\
+sass src/sass/nlist.scss:dist/css/nlist.css &&\
+sass src/sass/nlist.scss:dist/css/nlist.min.css -s compressed
 ```
---
+++ src/js/nlist.js
@@ -0,0 +1,82 @@
+/*!
+ * NList 1.0.0
+ * https://nlist.zlatov.su
+ * NList may be freely distributed under the ISC license.
+ * 2025-03-22 17:08
+ */
+$(document).ready(function() {
+
+  $(".ul_to_tree.ul_to_tree-dropdown").on("click", "li", function(event) {
+    event.stopPropagation()
+    // console.log('event: ', event)
+    // console.log('event.offsetX: ', event.offsetX)
+    // console.log('event.offsetY: ', event.offsetY)
+    // console.log('$(this).offsetWidth: ', $(this).offsetWidth)
+    // if ($(this).nextAll("ul").css('display')=='none') {
+    //   $(this).nextAll("ul").slideDown(400)
+    //   $(this).css({'background-position':"-11px 0"})
+    // } else {
+    //   $(this).nextAll("ul").slideUp(400)
+    //   $(this).css({'background-position':"0 0"})
+    // }
+    if (event.offsetX >= -6 && event.offsetX <= 5 && event.offsetY >= 5 && event.offsetY <= 16 ) {
+      var li = $(this)
+      var elBlock = li.find(">ul")[0]
+      if (elBlock.style.height === "0px") {
+        elBlock.style.height = `${elBlock.scrollHeight}px`
+      } else {
+        elBlock.style.height = `${elBlock.scrollHeight}px`
+        window.getComputedStyle(elBlock, null).getPropertyValue("height")
+        elBlock.style.height = "0"
+      }
+      li.toggleClass("close")
+    }
+  })
+
+  $(".ul_to_tree.ul_to_tree-dropdown ul").on("transitionend", function(event) {
+    var elBlock = $(this)[0]
+    if (elBlock.style.height !== "0px") {
+      elBlock.style.height = "auto"
+    }
+  })
+
+  // elToggle.addEventListener("click", () => {
+  //   if (elBlock.style.height === "0px") {
+  //     elBlock.style.height = `${ elBlock.scrollHeight }px`
+  //   } else {
+  //     elBlock.style.height = `${ elBlock.scrollHeight }px`
+  //     window.getComputedStyle(elBlock, null).getPropertyValue("height")
+  //     elBlock.style.height = "0"
+  //   }
+  // })
+
+  // elBlock.addEventListener("transitionend", () => {
+  //   if (elBlock.style.height !== "0px") {
+  //     elBlock.style.height = "auto"
+  //   }
+  // });
+
+  // $(".ul-dropfree").find("ul").slideUp(400).parents("li").children("div.drop").css({'background-position':"0 0"})
+
+  $(".ul_to_tree2.ul_to_tree2-dropdown").on("click", "li>i", function(event) {
+    event.stopPropagation()
+    var i = $(this)
+    var li = i.parent()
+    var elBlock = li.find(">ul")[0]
+    if (elBlock.style.height === "0px") {
+      elBlock.style.height = `${elBlock.scrollHeight}px`
+    } else {
+      elBlock.style.height = `${elBlock.scrollHeight}px`
+      window.getComputedStyle(elBlock, null).getPropertyValue("height")
+      elBlock.style.height = "0"
+    }
+    li.toggleClass("close")
+  })
+
+  $(".ul_to_tree2.ul_to_tree2-dropdown ul").on("transitionend", function(event) {
+    var elBlock = $(this)[0]
+    if (elBlock.style.height !== "0px") {
+      elBlock.style.height = "auto"
+    }
+  })
+})
