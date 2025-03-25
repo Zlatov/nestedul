04fc08f9a8e30b93869cd53fdd3b2a736c1c6329
zlatov <zlatov@ya.ru>
2025-03-25 14:54:41 +0300

    В будущем будем без jquery, вот наработка для начала;

--- test/index.js
+++ test/index.js
@@ -3,3 +3,7 @@
 $(document).ready(function() {
   NList.activate()
 })
+// https://tobiasahlin.com/blog/move-from-jquery-to-vanilla-javascript/
+// document.addEventListener("DOMContentLoaded", function(event) { 
+//   NList.activate()
+// })
