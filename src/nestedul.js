db7254a2203df44f240357ad3118f3d669e3d132
zlatov <zlatov@ya.ru>
2026-02-28 15:47:38 +0300

    избавиться от js + easy версия

--- src/nestedul.js
+++ src/nestedul.js
@@ -4,62 +4,56 @@
  * 2026-02-28 10:38
  */
 
-const NestedUl = {
-  activate: function() {
-    const lists = document.querySelectorAll("ul.nestedul")
-    console.log('lists: ', lists)
+export const version = "2.0.0"
+
+function click_handle(event) {
+  event.stopPropagation()
+  var i = $(this)
+  // Проверяем, кликнули ли по первому тегу <i></i> или по какому-либо в тексте узла.
+  if (i.index() != 0) {
+    return null
+  }
+
+  var li = $(this).parent()
+  var elBlock = li.find(">ul")[0]
+
+  // Чиню раскрытие дочерних узлов со стартовой позиции с классом nestedul-close.
+  if (li.hasClass("nestedul-close") && elBlock.style.height == "") {
+    elBlock.style.height = "0"
+  }
+
+  if (elBlock.style.height === "0px") {
+    elBlock.style.height = `${elBlock.scrollHeight}px`
+  } else {
+    elBlock.style.height = `${elBlock.scrollHeight}px`
+    window.getComputedStyle(elBlock, null).getPropertyValue("height")
+    elBlock.style.height = "0"
+  }
+
+  li.toggleClass("nestedul-close")
+}
+
+function transitionend_handle(event) {
+  const elBlock = event.target
+  if (elBlock.style.height !== "0px") {
+    elBlock.style.height = "auto"
   }
 }
 
-export default NestedUl
+export function activate(root = document) {
+  root.querySelectorAll("ul.nestedul-dropdown").forEach(function(ul, _i, _a) {
+    ul.addEventListener("click", function(event) {
+      if (event.target.matches("li > i")) {
+        click_handle.call(event.target, event)
+      }
+    })
+  })
+  root.querySelectorAll("ul.nestedul-dropdown ul").forEach(function(ul, _i, _a) {
+    ul.addEventListener("transitionend", transitionend_handle)
+  })
+}
 
+export function destroy() {
+}
 
-// NestedUl.activate = function() {
-//   $("body").on("click", ".nestedul-dropdown li>i", function(event) {
-//     event.stopPropagation()
-//     var i = $(this)
-//     // Проверяем, кликнули ли по первому тегу <i></i> или по какому-либо в тексте узла.
-//     if (i.index() != 0) {
-//       return null
-//     }
-//     var li = $(this).parent()
-//     var elBlock = li.find(">ul")[0]
-//     // Чиню раскрытие дочерних узлов со стартовой позиции с классом nestedul-close.
-//     if (li.hasClass("nestedul-close") && elBlock.style.height == "") {
-//       elBlock.style.height = "0"
-//     }
-//     if (elBlock.style.height === "0px") {
-//       // console.log('> ul === "0px"')
-//       elBlock.style.height = `${elBlock.scrollHeight}px`
-//       // console.log(`> height = ${elBlock.scrollHeight}px`)
-
-//       // if (elBlock.nestedul_timeout_id) {
-//       //   clearTimeout(elBlock.nestedul_timeout_id)
-//       // }
-//       // elBlock.nestedul_timeout_id = setTimeout(function() {
-//       //   elBlock.style.height = "auto" 
-//       //   // console.log('> height = auto')
-//       // }, 250)
-//     } else {
-//       // console.log('> ul !== "0px"')
-//       elBlock.style.height = `${elBlock.scrollHeight}px`
-//       // console.log(`> height = ${elBlock.scrollHeight}px`)
-//       window.getComputedStyle(elBlock, null).getPropertyValue("height")
-//       // console.log('> getComputedStyle')
-//       elBlock.style.height = "0"
-//       // console.log('> height = "0"')
-
-//       // if (elBlock.nestedul_timeout_id) {
-//       //   clearTimeout(elBlock.nestedul_timeout_id)
-//       // }
-//     }
-//     li.toggleClass("nestedul-close")
-//   })
-
-//   $("body .nestedul-dropdown ul").on("transitionend", function(event) {
-//     var elBlock = $(this)[0]
-//     if (elBlock.style.height !== "0px") {
-//       elBlock.style.height = "auto"
-//     }
-//   })
-// }
+export default { version, activate, destroy }
